33d652f750f0ce831fb64888cf353a7eb6cec67f
benjaminbis <benjaminbismuth2@gmail.com>
2024-12-04 15:32:50 +0200

    Update ArrCharOps.java

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -1,14 +1,13 @@
-
 public class ArrCharOps {
     public static void main(String[] args) {
         String str = "clearly";
         char[] arr1 = {'c','l','e','a','r','l','y'};
         char[] arr2 = {'U','n','d','e','r','s','t', 'o', 'o', 'd'};
-        System.out.println(str);  
-        println(arr1);            
-        System.out.println(charAt(arr1,2));      
-        System.out.println(indexOf(arr1,'l'));  
-        System.out.println(indexOf(arr1,'l',3)); 
+        System.out.println(str);
+        println(arr1);
+        System.out.println(charAt(arr1, 2));
+        System.out.println(indexOf(arr1, 'l'));
+        System.out.println(indexOf(arr1, 'l', 3));
         System.out.println(lastIndexOf(arr1, 'l'));
         System.out.println(concat(arr1, arr2));
         System.out.println(subArray(arr2, 2, 9));
@@ -23,7 +22,6 @@ public class ArrCharOps {
         System.out.println(hashCode(arr2));
     }
 
-   
     public static void println(char[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
@@ -31,12 +29,10 @@ public class ArrCharOps {
         System.out.println();
     }
 
-    
     public static char charAt(char[] arr, int index) {
         return arr[index];
     }
 
-    
     public static boolean equals(char[] arr1, char[] arr2) {
         if (arr1.length != arr2.length) return false;
         for (int i = 0; i < arr1.length; i++) {
@@ -45,7 +41,6 @@ public class ArrCharOps {
         return true;
     }
 
-    
     public static int indexOf(char[] arr, char ch) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == ch) return i;
@@ -53,7 +48,6 @@ public class ArrCharOps {
         return -1;
     }
 
- 
     public static int indexOf(char[] arr, char ch, int fromIndex) {
         for (int i = fromIndex; i < arr.length; i++) {
             if (arr[i] == ch) return i;
@@ -61,7 +55,6 @@ public class ArrCharOps {
         return -1;
     }
 
-    
     public static int lastIndexOf(char[] arr, char ch) {
         for (int i = arr.length - 1; i >= 0; i--) {
             if (arr[i] == ch) return i;
@@ -69,7 +62,6 @@ public class ArrCharOps {
         return -1;
     }
 
-    
     public static char[] concat(char[] arr1, char[] arr2) {
         char[] result = new char[arr1.length + arr2.length];
         for (int i = 0; i < arr1.length; i++) {
@@ -81,7 +73,6 @@ public class ArrCharOps {
         return result;
     }
 
-     
     public static char[] subArray(char[] arr, int beginIndex, int endIndex) {
         char[] result = new char[endIndex - beginIndex];
         for (int i = 0; i < result.length; i++) {
@@ -90,7 +81,6 @@ public class ArrCharOps {
         return result;
     }
 
-   
     public static long hashCode(char[] arr) {
         long hash = 0;
         long power = 1;
@@ -102,12 +92,18 @@ public class ArrCharOps {
     }
 
     public static int compareTo(String str1, String str2) {
-        int len = Math.min(str1.length(), str2.length());
-        for (int i = 0; i < len; i++) {
+        int len1 = str1.length();
+        int len2 = str2.length();
+        int minLength = Math.min(len1, len2);
+
+        for (int i = 0; i < minLength; i++) {
             char ch1 = str1.charAt(i);
             char ch2 = str2.charAt(i);
-            if (ch1 != ch2) return ch1 - ch2;
+            if (ch1 != ch2) {
+                return ch1 - ch2;
+            }
         }
-        return str1.length() - str2.length();
+
+        return len1 - len2;
     }
 }
